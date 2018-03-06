a4007d2caf9f59d9435b2c56e6a15044b0a52882
Rennan Cockles <rennancockles@gmail.com>
2018-03-06 16:21:39 -0300

    botao para cancelar o upload e barra de progresso funcionando

--- app/controller/mainController.py
+++ app/controller/mainController.py
@@ -1,10 +1,22 @@
 # -*- coding: utf-8 -*-
 
 from PyQt4 import QtGui, QtCore
-
+from poster.encode import multipart_encode
 import requests
 from os import path
 from view import mainView
+
+
+class IterableToFileAdapter(object):
+    def __init__(self, iterable):
+        self.iterator = iter(iterable)
+        self.length = iterable.total
+
+    def read(self, size=-1):
+        return next(self.iterator, b'')
+
+    def __len__(self):
+        return self.length
 
 
 class Upload(QtCore.QThread):
@@ -16,27 +28,29 @@ class Upload(QtCore.QThread):
         self.file = parent.file
         self.stop_thread = False
 
+    def multipart_encode_for_requests(self, params, boundary=None, cb=None):
+        datagen, headers = multipart_encode(params, boundary, cb)
+        return IterableToFileAdapter(datagen), headers
+
+    def progress(self, param, current, total):
+        if not param:
+            return
+
+        if self.stop_thread:
+            self.setTerminationEnabled(True)
+            self.emit(QtCore.SIGNAL('sigUPDATE(QString)'), '0')
+            self.terminate()
+            return
+
+        percent = str(current * 100 / total)
+        self.emit(QtCore.SIGNAL('sigUPDATE(QString)'), percent)
+
     def run(self):
-        pgr = 0
-        step = 10000
         url = 'http://localhost:8000/api/upload'
-        files = {'docfile': open(self.file['path'], 'rb')}
+        file = {'docfile': open(self.file['path'], 'rb')}
+        datagen, headers = self.multipart_encode_for_requests(file, cb=self.progress)
 
-        r = requests.post(url, files=files)
-
-        while pgr < self.file['size']:
-            if self.stop_thread:
-                self.terminate()
-                self.emit(QtCore.SIGNAL('sigUPDATE(QString)'), '0')
-
-            pgr += step
-            value = 100*pgr/self.file['size']
-
-            if value >= 100:
-                break
-
-            self.emit(QtCore.SIGNAL('sigUPDATE(QString)'), str(value))
-            self.sleep(1)
+        r = requests.post(url, data=datagen, headers=headers)
 
         self.emit(QtCore.SIGNAL('sigUPDATE(QString)'), '100')
 
@@ -80,14 +94,15 @@ class Main(QtGui.QMainWindow, mainView.Ui_MainWindow):
 
     def open_file(self):
         self.progressBar.setProperty("value", 0)
-        fname = QtGui.QFileDialog.getOpenFileName(self, 'Abrir Arquivo', '', '')
+        fname = QtGui.QFileDialog.getOpenFileName(self, 'Abrir Arquivo', '', '').toUtf8()
+        filename = str(fname).decode('utf8', 'ignore')
 
         if not fname:
             return
 
-        self.file['name'] = path.basename(str(fname))
-        self.file['path'] = str(fname)
-        self.file['size'] = path.getsize(fname)
+        self.file['name'] = path.basename(filename)
+        self.file['path'] = filename
+        self.file['size'] = path.getsize(filename)
 
         self.lb_filename.setText(self.file['name'])
 
